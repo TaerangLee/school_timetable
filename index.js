2276e2ee0916d813e6639debc82dd6e93d0adb13
TAERANG <s22051@gsm.hs.kr>
2023-01-08 00:34:23 +0900

    update the school timetable

---
+++ index.js
@@ -0,0 +1,7 @@
+const date = document.querySelector(".date");
+const grade = document.querySelector(".grade");
+const class1 = document.querySelector(".class"); //class 그 자체로 변수명 설정 금지
+const semester = document.querySelector(".semester");
+
+const regExp = /[0-9]/g;
+
