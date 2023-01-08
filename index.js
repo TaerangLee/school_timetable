d4fddaa3f6b649f8d9a6b9a922983cb65dd90b75
TAERANG <s22051@gsm.hs.kr>
2023-01-08 17:51:36 +0900

    api 구현

--- index.js
+++ index.js
@@ -9,33 +9,53 @@ const regExp = /[0-9]/g;
 
 
 submitInformation.addEventListener("click", () => {
-    const dateValue = date.value;
-    const gradeValue = grade.value;
-    const class1Value = class1.value;
-    const semesterValue = semester.value;
-    const submitInformationValue = submitInformation.value;
+    alert("오늘의 시간은?");
+    
+    const YMD = date.value;
+    const GRADE = grade.value;
+    const CLASS = class1.value;
+    const SEM = semester.value;
+
+    console.log(parseInt(YMD));
+    console.log(parseInt(GRADE));
+    console.log(parseInt(CLASS));
+    console.log(parseInt(SEM));
+
+    
     
 
-    const set = () => {
-        const url = `https://open.neis.go.kr/hub/hisTimetable?KEY=00da5e76d41248ee8a85992a899a8f63&Type=json&pIndex=1&pSize=10&ATPT_OFCDC_SC_CODE=F10&SD_SCHUL_CODE=7380292&AY=2022&SEM=${semesterValue}&ALL_TI_YMD=${dateValue}&GRADE=${gradeValue}&CLASS_NM=${class1Value}`;
-        fetch(url)
+    const SET = () => {
+        // const url = `https://open.neis.go.kr/hub/hisTimetable?KEY=00da5e76d41248ee8a85992a899a8f63&Type=json&pIndex=1&pSize=10&ATPT_OFCDC_SC_CODE=F10&SD_SCHUL_CODE=7380292&AY=2022&SEM=${SEM}&ALL_TI_YMD=${YMD}&GRADE=${GRADE}&CLASS_NM=${CLASS}`;
+        fetch(`https://open.neis.go.kr/hub/hisTimetable?KEY=00da5e76d41248ee8a85992a899a8f63&Type=json&pIndex=1&pSize=10&ATPT_OFCDC_SC_CODE=F10&SD_SCHUL_CODE=7380292&AY=2022&SEM=${SEM}&ALL_TI_YMD=${YMD}&GRADE=${GRADE}&CLASS_NM=${CLASS}`)
             .then((res) => res.json())
             .then((json) => {
-                
-        
-            });
-        
-    }
+                for (let i = 0; i < 7; i++) {
+                    const classOne = document.querySelector(".One");
+                    const classTwo = document.querySelector(".Two");
+                    const classThree = document.querySelector(".Three");
+                    const classFour = document.querySelector(".Four");
+                    const classFive = document.querySelector(".Five");
+                    const classSix = document.querySelector(".Six");
+                    const classSeven = document.querySelector(".Seven");
 
-
-
-
-
-
-
+               
+                    try {
+                        classOne.innerText = `${dateValue} ${i + 1}교시 ${json.hisTimeTable[1].row[i].ITRT_CNTNT}`;
+                    }
+                    catch {
+                        classOne.innerText = `${dateValue} ${i + 1}교시 수업없음`;
+                    }
+                }
+            })
+        }
 });
+            
+        
+     
 
-
-
-
-
+                
+                 
+        
+   
+        
+    
