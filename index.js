8dbf9ce9096306f4baf2844652cf06fa43f0440f
TAERANG <s22051@gsm.hs.kr>
2023-01-08 22:37:27 +0900

    api 기능 구현

--- index.js
+++ index.js
@@ -1,61 +1,57 @@
-
-
 const date = document.querySelector(".date");
 const grade = document.querySelector(".grade");
 const class1 = document.querySelector(".class"); //class 그 자체로 변수명 설정 금지
 const semester = document.querySelector(".semester");
-const submitInformation = document.querySelector(".submitInformation");
-const regExp = /[0-9]/g;
+const search = document.getElementById(`BT`);
 
+let YMD = 0;
+let GRADE = 0;
+let CLASS = 0;
+let SEM = 0;
+const TIME = 0;
 
-submitInformation.addEventListener("click", () => {
-    alert("오늘의 시간은?");
-    
-    const YMD = date.value;
-    const GRADE = grade.value;
-    const CLASS = class1.value;
-    const SEM = semester.value;
+const SET = () => {
 
-    console.log(parseInt(YMD));
-    console.log(parseInt(GRADE));
-    console.log(parseInt(CLASS));
-    console.log(parseInt(SEM));
-
-    
-    
-
-    const SET = () => {
-        // const url = `https://open.neis.go.kr/hub/hisTimetable?KEY=00da5e76d41248ee8a85992a899a8f63&Type=json&pIndex=1&pSize=10&ATPT_OFCDC_SC_CODE=F10&SD_SCHUL_CODE=7380292&AY=2022&SEM=${SEM}&ALL_TI_YMD=${YMD}&GRADE=${GRADE}&CLASS_NM=${CLASS}`;
-        fetch(`https://open.neis.go.kr/hub/hisTimetable?KEY=00da5e76d41248ee8a85992a899a8f63&Type=json&pIndex=1&pSize=10&ATPT_OFCDC_SC_CODE=F10&SD_SCHUL_CODE=7380292&AY=2022&SEM=${SEM}&ALL_TI_YMD=${YMD}&GRADE=${GRADE}&CLASS_NM=${CLASS}`)
-            .then((res) => res.json())
-            .then((json) => {
-                for (let i = 0; i < 7; i++) {
-                    const classOne = document.querySelector(".One");
-                    const classTwo = document.querySelector(".Two");
-                    const classThree = document.querySelector(".Three");
-                    const classFour = document.querySelector(".Four");
-                    const classFive = document.querySelector(".Five");
-                    const classSix = document.querySelector(".Six");
-                    const classSeven = document.querySelector(".Seven");
-
-               
-                    try {
-                        classOne.innerText = `${dateValue} ${i + 1}교시 ${json.hisTimeTable[1].row[i].ITRT_CNTNT}`;
-                    }
-                    catch {
-                        classOne.innerText = `${dateValue} ${i + 1}교시 수업없음`;
-                    }
-                }
-            })
+  fetch(
+    `https://open.neis.go.kr/hub/hisTimetable?KEY=00da5e76d41248ee8a85992a899a8f63&Type=json&pIndex=1&pSize=10&ATPT_OFCDC_SC_CODE=F10&SD_SCHUL_CODE=7380292&AY=2022&SEM=${SEM}&ALL_TI_YMD=${YMD}&GRADE=${GRADE}&CLASS_NM=${CLASS}`
+  ,)
+    .then((res) => res.json())
+    .then((json) => {
+      for (let i = 0; i < 7; i++) {
+          const inner = document.getElementById(`${i}`);
+        try {
+          inner.innerText = `${i + 1}교시 ${
+            json.hisTimetable[1].row[i].ITRT_CNTNT
+          }`;
+        } catch {
+          inner.innerText = `${i + 1}교시 수업없음`;
         }
-});
-            
-        
-     
+      }
+    })
+};
 
-                
-                 
-        
+const handleChange = (event) => {
+  if (event.target == grade) {
+    GRADE = event.target.value;
+  }
+  if (event.target == date) {
+    YMD = event.target.value;
+  }
+  if (event.target == class1) {
+    CLASS = event.target.value;
+  }
+  if (event.target == semester) {
+    SEM = event.target.value;
+    }
    
-        
-    
+};
+
+search.addEventListener("click", SET);
+
+
+class1.addEventListener("change", handleChange);
+date.addEventListener("change", handleChange);
+semester.addEventListener("change", handleChange);
+grade.addEventListener("change", handleChange);   
+
+
